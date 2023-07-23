8d30ad12d31711883572ede2cea631f5bab21ee7
Kursad <kursad.demirdogen@gmail.com>
2023-07-23 17:12:29 +0300

    auto mapper config

---
+++ ApiConsume/HotelProject.WebApi/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.WebApi.Mapping
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<RoomAddDto, Room>();
+            CreateMap<Room, RoomAddDto>();
+            
+            //reversemap dediğimiz zaman tersiniz tekrar yazmamıza gerek yok iki taraflı olarak map ediyor
+            CreateMap<UpdateRoomDto, Room>().ReverseMap();
+        }
+    }
+}
